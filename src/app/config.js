e9793302c471e946a9fd3089e8eb9922ee2a6eef
aaditn <aadit.noronha@gmail.com>
2023-05-28 13:43:21 -0700

    delete config

---
+++ src/app/config.js
@@ -0,0 +1,6 @@
+const config = {
+    hackerEarthAuthId: '70d786f634c438ecddb8336b4b7816bbc76d141c00a6.api.hackerearth.com',
+    hackerEarthSecretKey: '7c93441173957850cdd7eb115aa1cda2254b6442',
+  };
+  
+  export default config;
