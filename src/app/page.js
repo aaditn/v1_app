ed2eb65f6da5aa3dc708a4c85c4ca824ee6bb930
aaditn <aadit.noronha@gmail.com>
2023-05-31 10:19:41 -0700

    orig. commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 "use client";
-
 import React, { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
+import { Helmet } from 'react-helmet';
+
 import "./lessons.css";
+import unit from "./unit.css"; 
 
 let Title = [
   ["Printing Hello World", "Introduction to Variables", "Calculator!"],
@@ -19,6 +21,7 @@ let vidTitle = [
   ["Loops", "Arrays", "All Coming Together"]
 ];
 let unitTitle = [
+  "",
   "Introduction to Java",
   "Conditionals",
   "Methods and Constructors",
@@ -27,27 +30,53 @@ let unitTitle = [
 ];
 
 const MyComponent = () => {
-  const [currentUnit, setCurrentUnit] = useState(() => {
-    const storedUnit = sessionStorage.getItem('currentUnit');
-    return storedUnit ? parseInt(storedUnit) : 1;
-  });
-  const [currentLesson, setCurrentLesson] = useState(() => {
-    const storedLesson = sessionStorage.getItem('currentLesson');
-    return storedLesson ? parseInt(storedLesson) : 1;
-  });
+  const [currentUnit, setCurrentUnit] = useState(1);
+  const [currentLesson, setCurrentLesson] = useState(1);
   const [outputData, setOutputData] = useState('');
 
   const editorRef = useRef(null);
 
   useEffect(() => {
     let editor = null;
-    const initializeAceEditor = () => {
-      const ace = require('ace-builds/src-noconflict/ace');
-      require('ace-builds/src-noconflict/theme-monokai');
-      require('ace-builds/src-noconflict/mode-java');
-      editorRef.current.editor = ace.edit(editorRef.current);
-      editorRef.current.editor.setTheme('ace/theme/monokai');
-      editorRef.current.editor.session.setMode('ace/mode/java');
+
+    if (currentUnit !== 0) {
+      const initializeAceEditor = () => {
+        const ace = require('ace-builds/src-noconflict/ace');
+        require('ace-builds/src-noconflict/theme-monokai');
+        require('ace-builds/src-noconflict/mode-java');
+        editorRef.current.editor = ace.edit(editorRef.current);
+        editorRef.current.editor.setTheme('ace/theme/monokai');
+        editorRef.current.editor.session.setMode('ace/mode/java');
+        editorRef.current.editor.setValue(`
+public class Main {
+  public static void main(String[] args) {
+    // Write your code here
+
+  }
+}`);
+        editorRef.current.editor.clearSelection();
+      };
+
+      initializeAceEditor();
+    }
+
+    return () => {
+      if (editorRef.current.editor) {
+        editorRef.current.editor.destroy();
+      }
+    };
+  }, [currentUnit]);
+
+  const incrementUnit = () => {
+    setCurrentUnit((prevUnit) => {
+      const newUnit = prevUnit + 1;
+      return newUnit;
+    });
+    setCurrentLesson(1);
+    setOutputData('');
+
+    // Reset code editor
+    if (editorRef.current.editor) {
       editorRef.current.editor.setValue(`
 public class Main {
   public static void main(String[] args) {
@@ -56,35 +85,17 @@ public class Main {
   }
 }`);
       editorRef.current.editor.clearSelection();
-    };
-
-    initializeAceEditor();
-
-    return () => {
-      if (editorRef.current.editor) {
-        editorRef.current.editor.destroy();
-      }
-    };
-  }, []);
-
-  const incrementUnit = () => {
-    setCurrentUnit((prevUnit) => {
-      const newUnit = prevUnit + 1;
-      sessionStorage.setItem('currentUnit', newUnit.toString());
-      return newUnit;
-    });
-    setCurrentLesson(1);
-    sessionStorage.setItem('currentLesson', '1');
-    window.location.reload();
+    }
   };
 
   const incrementLesson = () => {
     setCurrentLesson((prevLesson) => {
       let newLesson;
       let newUnit = currentUnit;
-  
+
       if (prevLesson === 3) {
         newLesson = 1;
+        incrementUnit();
         if (currentUnit !== 5) {
           newUnit = currentUnit + 1;
         } else {
@@ -93,14 +104,42 @@ public class Main {
       } else {
         newLesson = prevLesson + 1;
       }
-  
-      sessionStorage.setItem('currentLesson', newLesson.toString());
-      sessionStorage.setItem('currentUnit', newUnit.toString());
+
       return newLesson;
     });
-  
-    window.location.reload();
+
+    setOutputData('');
+
+    // Reset code editor
+    if (editorRef.current.editor) {
+      editorRef.current.editor.setValue(`
+public class Main {
+  public static void main(String[] args) {
+    // Write your code here
+
+  }
+}`);
+      editorRef.current.editor.clearSelection();
+    }
   };
+  const goToLesson = (unit, lesson) => {
+    setCurrentLesson(lesson);
+    setCurrentUnit(unit);
+    setOutputData('');
+  
+    // Reset code editor
+    if (editorRef.current.editor) {
+      editorRef.current.editor.setValue(`
+  public class Main {
+    public static void main(String[] args) {
+      // Write your code here
+  
+    }
+  }`);
+      editorRef.current.editor.clearSelection();
+    }
+  };
+  
 
   const executeCode = async () => {
     try {
@@ -120,49 +159,67 @@ public class Main {
         }
       }
     } catch (error) {
-      console.error('Execution failed. Error:', error);
+      console.error('Execution failed. Error:', error.response.data);
     }
   };
 
   const lessonPage = () => {
+
+
     return (
+      
+      <>
       <div>
-        <div className="header">
-          <h3>Project Code</h3>
-          <h3>Unit {currentUnit}: {unitTitle[currentUnit - 1]}</h3>
-        </div>
 
-        <div className="next">
-          <button className="next" onClick={incrementLesson}>Next→</button>
-        </div>
+        <Helmet>
+          <link rel="stylesheet" href="./lessons.css" />
+          <meta charSet="utf-8" />
+          <title>ProjectCode.edu</title>
+        </Helmet>
 
-        <div className="title">
-          <h1>Lesson {currentLesson}</h1>
-          <h2>{Title[currentUnit - 1][currentLesson - 1]}</h2>
-        </div>
-
-        <div className="video">
-          <h1>{vidTitle[currentUnit - 1][currentLesson - 1]}</h1>
-        </div>
-
-        <div>
-          <button onClick={executeCode}>Run</button>
-        </div>
-        <div className="compiler">
-          <h1>Run Your Code</h1>
-          <br></br>
-          <div ref={editorRef} className="box"></div>
-          <div className="console">
-            <div className="consoleHeader">
-              Console
-            </div>
-            {outputData && <pre>{outputData}</pre>}
+        <body>
+          <div className="header">
+            <h3>Project Code</h3>
+            <h3>Unit {currentUnit}: {unitTitle[currentUnit]}</h3>
           </div>
+
+          <div className="next">
+            <button className="next" onClick={incrementLesson}>Next→</button>
+          </div>
+
+          <div className="title">
+            <h1>Lesson {currentLesson}</h1>
+            <h2>{Title[currentUnit - 1][currentLesson - 1]}</h2>
+          </div>
+
+          <div className="video">
+            <h1>{vidTitle[currentUnit - 1][currentLesson - 1]}</h1>
+          </div>
+
+          <div>
+            <button onClick={executeCode}>Run</button>
+          </div>
+          <div className="compiler">
+            <h1>Run Your Code</h1>
+            <br></br>
+            <div ref={editorRef} className="box"></div>
+            <div className="console">
+              <div className="consoleHeader">
+                Console
+              </div>
+              {outputData && <pre>{outputData}</pre>}
+            </div>
+          </div>
+        </body>
         </div>
-      </div>
+      </>
     );
   };
-  return lessonPage();
+
+
+  return (
+    unitPage()
+  );
 };
 
 export default MyComponent;
